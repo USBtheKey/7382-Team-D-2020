0007226c7c1701bbdbbaf38d718d4c7c59f699fd
USBtheKey <38596161+USBtheKey@users.noreply.github.com>
2020-02-13 02:01:35 -0500

    Update

--- Horror Escape Puzzle Game/Assets/Scripts/Gameplay/Platforms/Platform_Behavior.cs
+++ Horror Escape Puzzle Game/Assets/Scripts/Gameplay/Platforms/Platform_Behavior.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+
+
+[RequireComponent( typeof(BoxCollider2D))]
+public class Platform_Behavior : MonoBehaviour
 {
+
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float speed;
+    [SerializeField] private float stopTime;
+
+
+    private Rigidbody2D platRigid;
+    private Collider2D platColl;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        platColl = this.gameObject.GetComponent<BoxCollider2D>();
+        //platRigid = this.gameObject.GetComponent<Rigidbody2D>();
+
+        this.gameObject.transform.position = waypoints[0].position;
+
+        MovePlatform();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MovePlatform()
     {
+        while (true)
+        {
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                Vector2 direction = (waypoints[i].position - this.gameObject.transform.position).normalized;
+                gameObject.transform.Translate(direction * speed * Time.deltaTime);
+                Debug.Log(i);
+            }
+
+            for (int i = waypoints.Length; i > 0; i--)
+            {
+                Vector2 direction = (waypoints[i].position - this.gameObject.transform.position).normalized;
+                gameObject.transform.Translate(waypoints[i].position * speed * Time.deltaTime);
+            }
+        }
         
     }
+    
 }
--- Horror Escape Puzzle Game/Assets/Scripts/Gameplay/Traps/Saw_Behavior.cs
+++ Horror Escape Puzzle Game/Assets/Scripts/Gameplay/Traps/Saw_Behavior.cs
@@ -9,22 +9,17 @@ public class Saw_Behavior : MonoBehaviour
 {
 
     #region UnityEditor
-    [SerializeField] private enum Behavior { Default, Translate }
+    [SerializeField] private enum Behavior { Default, Translate, Orbit }
     [SerializeField] private Behavior behavior;
-
     [SerializeField] private Transform[] waypoints;
     [Range(0f,10f)][SerializeField] private float speed;
-
     [Tooltip("Do you want the Saw to follow the path forever?")]
-    [SerializeField] private bool loop;
-
     [HideInInspector] private int targetWaypoint = 0;
-
     [HideInInspector] private LineRenderer path;
 
 
-
-
+    //For the saw orbit
+    [SerializeField] private Transform anchor;
     #endregion
 
 
@@ -32,75 +27,75 @@ public class Saw_Behavior : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        Debug.Log(behavior.ToString());
+
+
 
         path = this.GetComponent<LineRenderer>();
 
         path.positionCount = waypoints.Length;
-        if (loop) path.positionCount += 1;
+        if (behavior == Behavior.Translate) path.positionCount ++;
 
         path.startColor = path.endColor = Color.white;
         path.startWidth = path.endWidth = 0.1f;
         DrawPath();
+        
+
+        Debug.Log(behavior.ToString());
+
     }
 
     private void DrawPath()
     {
-        for (int i = 0; i < waypoints.Length; i++)
+
+        if(behavior == Behavior.Translate)
         {
-            path.SetPosition(i,waypoints[i].position);
-        }
-    
-        if (loop) 
-        {
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                path.SetPosition(i, waypoints[i].position);
+            }
             path.SetPosition(path.positionCount - 1, waypoints[0].position);
         }
 
-
+        else if(behavior == Behavior.Orbit)
+        {
+            path.SetPosition(0, anchor.position);
+            path.SetPosition(1, this.gameObject.transform.position);
+        }
+        
+        
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        
-        switch (behavior)
+        if(behavior == Behavior.Translate)
         {
-            case Behavior.Default:
-                //Spins at placed location
-                break;
+            Vector2 direction = (waypoints[targetWaypoint].position - this.gameObject.transform.position).normalized; //Easier to manipulate when normalized.
+            Vector2 currentPos = this.transform.position;
+            Vector2 destination = waypoints[targetWaypoint].position;
 
-            case Behavior.Translate:
-
-                Vector2 direction = (waypoints[targetWaypoint].position - this.gameObject.transform.position).normalized; //Easier to manipulate when normalized.
-                Vector2 currentPos = this.transform.position;
-                Vector2 destination = waypoints[targetWaypoint].position;
-
-                if (Vector2.Distance(currentPos, destination) >= 0.1f)
-                {
-                    this.gameObject.transform.Translate(direction * speed * Time.deltaTime);
-                    //Debug.Log(Vector2.Distance(currentPos, destination));
-                }
-                else if(targetWaypoint + 1 < waypoints.Length) //prevent index out of range error
-                {
-                    //Previous waypoint reached. Going to the next one.
-                    targetWaypoint++;
-                    Debug.Log("Waypoint: " + targetWaypoint);
-                }
-                else if (targetWaypoint == waypoints.Length - 1 && loop)
-                {
-                    targetWaypoint = 0;
-                    Debug.Log("Waypoint: " + targetWaypoint);
-                }
-                Debug.DrawRay(currentPos, waypoints[targetWaypoint].position - this.gameObject.transform.position);
-
-                break;
-
+            if (Vector2.Distance(currentPos, destination) >= 0.1f)
+            {
+                this.gameObject.transform.Translate(direction * speed * Time.deltaTime);
+                //Debug.Log(Vector2.Distance(currentPos, destination));
+            }
+            else if (targetWaypoint + 1 < waypoints.Length) //prevent index out of range error
+            {
+                //Previous waypoint reached. Going to the next one.
+                targetWaypoint++;
+                Debug.Log("Waypoint: " + targetWaypoint);
+            }
+            else if (targetWaypoint == waypoints.Length - 1)
+            {
+                targetWaypoint = 0;
+                Debug.Log("Waypoint: " + targetWaypoint);
+            }
+            Debug.DrawRay(currentPos, waypoints[targetWaypoint].position - this.gameObject.transform.position);
         }
+
     }
 
-
-    //TODO Implement the tracks if the Saw tanslates.
-    //TODO What if the saw orbits the anchor?
+    //TODO What if the saw orbits attached to an anchor?
 
 }
 
