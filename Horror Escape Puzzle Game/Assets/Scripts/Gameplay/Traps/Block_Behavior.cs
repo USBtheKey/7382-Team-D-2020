6b4d3177dd1acbbe054fe868faded864633f858c
USBtheKey <38596161+USBtheKey@users.noreply.github.com>
2020-02-25 01:37:05 -0500

    Update

--- Horror Escape Puzzle Game/Assets/Scripts/Gameplay/Traps/Block_Behavior.cs
+++ Horror Escape Puzzle Game/Assets/Scripts/Gameplay/Traps/Block_Behavior.cs
@@ -7,18 +7,47 @@ using UnityEngine;
 public class Block_Behavior : MonoBehaviour
 {
     [Header("Set Block visibility")]
-    [SerializeField] private bool visible;
+    [SerializeField] private bool startVisible;
+
+    [Tooltip("Number of times before the trap gets triggered")]
+    [SerializeField] private int triggerCounter;
+    private int counter;
 
     [HideInInspector] private SpriteRenderer sprite;
 
     private void Start()
     {
-        if (!visible) sprite.enabled = false; 
+        sprite = GetComponent<SpriteRenderer>();
+
+        if (!startVisible) sprite.enabled = false;
+        else sprite.enabled = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        this.gameObject.SetActive(false);
+        
+        if (counter == triggerCounter)
+        {
+            if (startVisible)
+            {
+                this.gameObject.SetActive(false);
+            }
+            else
+            {
+                sprite.enabled = true;
+                this.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            counter++;
+        }
+        
+    }
 
+    //Can make a trigger for this too;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        counter++;
     }
 }
